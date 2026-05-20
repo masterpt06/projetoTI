a11ef633aa7a0ac469ec54d43ac821523a65091f
danielsynchak <danielsynchak@gmail.com>
2026-05-20 17:47:44 +0100

    a tentar mostrar foto ao esta ser selecionada

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -14,26 +14,9 @@
                         'delete' => Gate::check('delete', $tshirtImage)
                         ])
 
-                <!--@if($tshirtImage->disciplines->isEmpty())
-                    <p class="mt-12 text-xl text-gray-700 dark:text-gray-300">
-                        This course has no assigned disciplines.
-                    </p>
-                @else
-                    @can('viewCurriculum', \App\Models\Course::class)
-                        <h3 class="pt-16 pb-4 text-xl font-medium text-gray-700 dark:text-gray-300">
-                            Curriculum
-                        </h3>
-                        <x-courses.curriculum :disciplines="$course->disciplines"
-                                            :showView="true"
-                                            class="pt-4"
-                        />
-                    @endcan
-                @endif-->
+                
             </section>
         </div>
     </div>
-    <form id="delete-form" method="POST" action="{{ route('courses.destroy', ['course' => $tshirtImage]) }}" class="hidden">
-        @csrf
-        @method('DELETE')
-    </form>
+    
 </x-layouts::main-content>
