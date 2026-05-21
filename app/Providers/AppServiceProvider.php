8e7497cd1c25a6ec1e1c057d06117b4e6acdb163
Pedro Fidalgo <pmfrazaofidalgo@gmail.com>
2026-05-21 09:08:50 +0100

    ProjetoBase

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,7 @@
 
 namespace App\Providers;
 
-use Carbon\CarbonImmutable;
-use Illuminate\Support\Facades\Date;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Validation\Rules\Password;
-use Illuminate\Support\Facades\View;
-use App\Models\Course;
-use App\Models\User;
-use Illuminate\Support\Facades\Gate;
-use App\Policies\AdministrativePolicy;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -28,51 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->configureDefaults();
-
-        Gate::policy(User::class, AdministrativePolicy::class);
-
-        Gate::define('use-cart', function (?User $user) {
-            return $user === null || $user->type == 'A' || $user->type == 'S';
-        });
-
-        Gate::define('confirm-cart', function (User $user) {
-            return $user->type == 'A' || $user->type == 'S';
-        });
-
-        Gate::define('admin', function (User $user) {
-            // Only "administrator" users can "admin"
-            return $user->admin;
-        });
-        try {
-            // View::share adds data (variables) that are shared through all views
-            View::share('sharedCourses', Course::orderBy('type')->orderBy('abbreviation')->get());
-        } catch (\Exception $e) {
-            // No need to do anything – this just ensures that no exception is
-            // thrown if "courses" table does not exist when running
-            // "php artisan migrate" for the first time
-        }
-    }
-
-    /**
-     * Configure default behaviors for production-ready applications.
-     */
-    protected function configureDefaults(): void
-    {
-        Date::use(CarbonImmutable::class);
-
-        DB::prohibitDestructiveCommands(
-            app()->isProduction(),
-        );
-
-        Password::defaults(fn (): ?Password => app()->isProduction()
-            ? Password::min(12)
-                ->mixedCase()
-                ->letters()
-                ->numbers()
-                ->symbols()
-                ->uncompromised()
-            : null,
-        );
+        //
     }
 }
